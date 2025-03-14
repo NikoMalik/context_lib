5f2a59c5b932e3572b0f24f9be74ce25055959ec
NikoMalik <123812634+NikoMalik@users.noreply.github.com>
2025-03-14 23:32:55 +0300

    value return half bytes{not working}

--- context.zig
+++ context.zig
@@ -75,7 +75,7 @@ pub const Context = struct {
     deadline: ?u64,
     parent: ?*Context,
     children: std.ArrayList(*Context),
-    values: ?std.StringHashMapUnmanaged(?*anyopaque) = null,
+    values: ?std.StringHashMap(?*anyopaque) = null,
     const Self = @This();
 
     pub fn init(
@@ -84,7 +84,7 @@ pub const Context = struct {
         const epfd = try posix.epoll_create1(0);
         errdefer posix.close(epfd); // check err
 
-        const cancel_fd = try posix.eventfd(0, linux.EFD.NONBLOCK);
+        const cancel_fd = try posix.eventfd(0, linux.EFD.CLOEXEC | linux.EFD.NONBLOCK);
 
         errdefer posix.close(cancel_fd); // check err
 
@@ -93,14 +93,29 @@ pub const Context = struct {
         });
         errdefer posix.close(timer_fd); // check err
 
-        var event = linux.epoll_event{
+        var event = EVENT{
             .events = linux.EPOLL.IN | linux.EPOLL.ET,
             .data = .{ .fd = cancel_fd },
         };
-        _ = linux.epoll_ctl(epfd, linux.EPOLL.CTL_ADD, cancel_fd, &event);
+        const err_ctl_1 = linux.epoll_ctl(epfd, linux.EPOLL.CTL_ADD, cancel_fd, &event);
+        if (err_ctl_1 == -1) {
+            posix.close(epfd);
+            posix.close(cancel_fd);
+            posix.close(timer_fd);
+            std.log.err("Epoll Create Failed: {d}", .{err_ctl_1});
+            return error.EPOLL_CTL_ERROR_creation;
+        }
 
         event.data.fd = @intCast(timer_fd);
-        _ = linux.epoll_ctl(epfd, linux.EPOLL.CTL_ADD, @intCast(timer_fd), &event);
+        const err_ctl_2_timer = linux.epoll_ctl(epfd, linux.EPOLL.CTL_ADD, @intCast(timer_fd), &event);
+        if (err_ctl_2_timer == -1) {
+            posix.close(epfd);
+            posix.close(cancel_fd);
+            posix.close(timer_fd);
+
+            std.log.err("Epoll Create Failed Timer :{d}", .{err_ctl_2_timer});
+            return error.EPOLL_CTL_ERROR_creation;
+        }
 
         return Self{
             .id = 0,
@@ -112,7 +127,7 @@ pub const Context = struct {
             .deadline = null,
             .cancelled = Atomic(bool).init(false),
             .children = std.ArrayList(*Context).init(allocator),
-            .values = std.StringHashMapUnmanaged(?*anyopaque){},
+            .values = std.StringHashMap(?*anyopaque).init(allocator),
         };
     }
 
@@ -137,12 +152,17 @@ pub const Context = struct {
 
         if (self.parent == null) { // if parent == null
             for (self.children.items) |child| { // cleanup children
+                // child.values.?.deinit(self.allocator);
+                if (child.values) |*values| {
+                    values.deinit();
+                }
                 child.deinit();
                 self.allocator.destroy(child);
             }
             self.children.deinit();
+
             if (self.values) |*vals| {
-                vals.deinit(self.allocator);
+                vals.deinit();
                 self.values = null;
             }
             posix.close(self.epfd);
@@ -152,11 +172,13 @@ pub const Context = struct {
 
             return;
         }
-
-        if (self.values) |*vals| {
-            vals.deinit(self.allocator);
-            self.values = null;
-        }
+        // ONLY PARENT CAN CLEANUP VALUES
+        // ================================//
+        // if (self.values) |*vals| {
+        //     vals.deinit(self.allocator);
+        //     self.values = null;
+        // }
+        // ================================//
 
         posix.close(self.epfd);
         posix.close(self.cancelFd);
@@ -170,9 +192,9 @@ pub const Context = struct {
         if (!self.cancelled.swap(true, .seq_cst)) {
             const val: u64 = 1;
             _ = posix.write(self.cancelFd, std.mem.asBytes(&val)) catch {};
-            for (self.children.items) |child| {
-                child.cancel();
-            }
+            // for (self.children.items) |child| {
+            //     child.cancel();
+            // }
         }
     }
 
@@ -208,55 +230,90 @@ pub const Context = struct {
         value: ?*anyopaque,
     ) !void {
         if (self.values == null) {
-            self.values = std.StringHashMapUnmanaged(?*anyopaque){};
+            self.values = std.StringHashMap(?*anyopaque).init(self.allocator);
+        }
+        if (self.values.?.contains(key)) {
+            std.debug.print("Overwriting key '{s}' in context {d}\n", .{ key, self.id });
         }
         std.debug.print("Hash table size before put: {}\n", .{self.values.?.count()});
-        try self.values.?.put(self.allocator, key, value);
+        try self.values.?.put(key, value);
         std.debug.print("Hash table size after put: {}\n", .{self.values.?.count()});
     }
     pub fn getValue(
         self: *Self,
         key: []const u8,
     ) ?*anyopaque {
+        if (self.cancelled.load(.acquire)) {
+            std.log.err("Context cancelled, values destroyed\n", .{});
+            return null;
+        }
+        var visited = std.AutoHashMap(*Context, void).init(self.allocator);
+        defer visited.deinit();
+
+        return self.getValueRecursive(key, &visited);
+    }
+
+    fn getValueRecursive(
+        self: *Self,
+        key: []const u8,
+        visited: *std.AutoHashMap(*Context, void),
+    ) ?*anyopaque {
+        std.debug.print("Check Context: {d}, key: {s}\n", .{ self.id, key });
+        if (visited.contains(self)) return null;
+        visited.put(self, {}) catch return null;
+        visited.put(self, {}) catch |err| {
+            std.log.err(" visited: {}", .{err});
+            return null;
+        };
+
         if (self.values) |values| {
-            if (values.get(key)) |value| {
-                return value;
+            if (values.get(key)) |val| {
+                return val;
             }
         }
+
         if (self.parent) |parent| {
-            return parent.getValue(key);
+            return parent.getValueRecursive(key, visited);
         }
+
         return null;
     }
     pub fn wait(
         self: *Self,
         events: []linux.epoll_event,
     ) !usize {
-        const count = linux.epoll_wait(
-            self.epfd,
-            events.ptr,
-            @intCast(events.len),
-            -1,
-        );
-        if (count < 0) {
-            std.log.err("epoll_wait failed with errno: {}", .{posix.errno(-count)});
-            return error.EpollWaitFailed;
-        }
-        for (events[0..count]) |*ev| {
-            const fd = ev.data.fd;
-            if (fd == self.cancelFd) {
-                var buf: [8]u8 = undefined;
-                _ = posix.read(self.cancelFd, &buf) catch {};
-                self.cancelled.store(true, .release);
-            } else if (fd == self.timerfd) {
-                var buf: [8]u8 = undefined;
-                _ = posix.read(self.timerfd.?, &buf) catch {};
+        while (true) {
+            const count = linux.epoll_wait(
+                self.epfd,
+                events.ptr,
+                @intCast(events.len),
+                -1,
+            );
+            if (count < 0) {
+                switch (posix.errno(count)) {
+                    .INTR => continue,
+                    else => |err| return posix.unexpectedErrno(err),
+                }
 
-                self.cancel();
+                return error.EpollWaitFailed;
             }
-        }
+            for (events[0..count]) |*ev| {
+                const fd = ev.data.fd;
+                if (fd == self.cancelFd) {
+                    var buf: [8]u8 = undefined;
+                    _ = posix.read(self.cancelFd, &buf) catch {};
 
-        return count;
+                    self.cancelled.store(true, .release);
+                } else if (fd == self.timerfd) {
+                    var buf: [8]u8 = undefined;
+                    _ = posix.read(self.timerfd.?, &buf) catch {};
+
+                    self.cancel();
+                }
+            }
+
+            return count;
+        }
     }
 
     fn eventLoop(
@@ -340,6 +397,33 @@ test "parent and child " {
     std.time.sleep(200_000_000); // 200ms
 }
 
+test "value propagation" {
+    var db = std.heap.DebugAllocator(.{}){};
+    defer _ = db.deinit();
+    const allocator = db.allocator();
+
+    var parent = try Context.init(allocator);
+    defer parent.deinit();
+
+    var child = try Context.withCancel(&parent);
+    defer child.deinit();
+
+    var test_value = "parent_value";
+    try parent.setValue("key", (@ptrCast(&test_value)));
+
+    const received = child.getValue("key");
+
+    if (received) |val_ptr| {
+        const str = @as(*[]const u8, @alignCast(@ptrCast(val_ptr))).*;
+        std.debug.print("value: {s}\n", .{str});
+        try std.testing.expectEqualStrings(test_value, str);
+    } else {
+        std.debug.print("No value found\n", .{});
+        try std.testing.expect(false);
+    }
+    return;
+}
+//
 fn long_running_task(ctx: *Context) !void {
     var i: u32 = 0;
     while (i < 10) : (i += 1) {
